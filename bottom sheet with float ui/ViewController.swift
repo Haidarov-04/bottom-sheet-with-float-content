5e5d0bfa252e147d94f63679e9bbda072daa30ad
haidarov_n <nozimjonhaydarov6@gmail.com>
2025-03-11 12:41:01 +0500

    Initial Commit

---
+++ bottom sheet with float ui/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bottom sheet with float ui
+//
+//  Created by Haidarov N on 3/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
